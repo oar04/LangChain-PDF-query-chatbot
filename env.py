76f898885abe67d12e1c1a969ecae21c46aad5cb
Osman Abdulrazak <60141020+oar04@users.noreply.github.com>
2023-10-27 10:28:45 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ.setdefault("OPENAI_API_KEY", "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")
-os.environ.setdefault("PINECONE_SECRET_KEY", "65edfe50-83a9-46fa-8a99-6718716c55c4")
-os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "gcp-starter")
+os.environ.setdefault("OPENAI_API_KEY", "own key")
+os.environ.setdefault("PINECONE_SECRET_KEY", "own key")
+os.environ.setdefault("PINECONE_ENVIRONMENT_REGION", "own key")
